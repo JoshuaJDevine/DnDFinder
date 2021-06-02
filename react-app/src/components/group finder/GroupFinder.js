201d72b4db4a58feb119c71502d09bf768da55fa
JoshuaJDevine <jd8386@gmail.com>
2021-06-02 09:47:33 -0700

    Styling Groups

--- react-app/src/components/group finder/GroupFinder.js
+++ react-app/src/components/group finder/GroupFinder.js
@@ -9,14 +9,46 @@ export  default function GroupFinder({groupList}){
         { id: 1, name: "John Doe" },
         { id: 2, name: "Victor Wayne" },
         { id: 3, name: "Jane Doe" },
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Victor Wayne" },
+        { id: 3, name: "Jane Doe" },
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Victor Wayne" },
+        { id: 3, name: "Jane Doe" },
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Victor Wayne" },
+        { id: 3, name: "Jane Doe" },
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Victor Wayne" },
+        { id: 3, name: "Jane Doe" },
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Victor Wayne" },
+        { id: 3, name: "Jane Doe" },
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Victor Wayne" },
+        { id: 3, name: "Jane Doe" },
+        { id: 1, name: "John Doe" },
+
 
     ];
 
     return (
     <div className="DnD__GroupFinder">
-      {groupList.map((group) => (
-        <GroupCard data={group} />
-      ))}
+      {groupList.map((group, idx) => {
+          if (idx % 3 === 0){
+            return(
+                <div className="DnD_GroupFinderCardBox">
+                      <GroupCard data={group} />
+                </div>
+            )
+          }
+          else {
+            return(
+              <GroupCard data={group} />
+            )
+          }
+      })
+  }
     </div>
     );
 }
--- react-app/src/components/home/Home.js
+++ react-app/src/components/home/Home.js
@@ -14,7 +14,7 @@ export default function Home(){
 
             <GroupFinder />
 
-            <BackgroundTester/>
+            {/*<BackgroundTester/>*/}
 
         </div>
     )
