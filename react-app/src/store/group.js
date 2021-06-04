8b224af10bbf1b52009e75d6afeb75b662a587ac
JoshuaJDevine <jd8386@gmail.com>
2021-06-04 16:59:58 -0700

    Bug Fixes

--- react-app/src/store/group.js
+++ react-app/src/store/group.js
@@ -41,7 +41,6 @@ export const getAllGroups = () => async (dispatch)  => {
       }
     });
     const data = await response.json();
-    console.log(data)
     if (data.errors) {
         return;
     }
