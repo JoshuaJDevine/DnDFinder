61689c79dd53d7c84f584749c58c6124825f3976
JoshuaJDevine <jd8386@gmail.com>
2021-06-06 09:57:28 -0700

    Started application thunks

---
+++ react-app/src/store/application.js
@@ -0,0 +1,8 @@
+//=========================================================
+//CONSTANTS
+//=========================================================
+const GET_APPLICATIONS = "session/GET_APPLICATIONS"
+const GET_APPLICATION = "session/GET_APPLICATION"
+const CREATE_APPLICATION = "session/CREATE_APPLICATION"
+const UPDATE_APPLICATION = "session/UPDATE_APPLICATION"
+const DELETE_APPLICATION = "session/DELETE_APPLICATION"
