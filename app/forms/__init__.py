d26bae6d2509db4fb781f7c4ae9d36d198c1f75d
JoshuaJDevine <jd8386@gmail.com>
2021-06-06 09:35:47 -0700

    Add application form

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .create_group_form import CreateGroupForm
 from .create_event_form import CreateEventForm
+from .application_form import ApplicationForm
