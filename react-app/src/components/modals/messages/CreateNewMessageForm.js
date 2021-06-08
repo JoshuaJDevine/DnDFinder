3a7992be905a430cf8d08850c911997378cbf244
JoshuaJDevine <jd8386@gmail.com>
2021-06-08 09:44:18 -0700

    Made new Message form and modal

---
+++ react-app/src/components/modals/messages/CreateNewMessageForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./CreateNewMessageForm.css"
+export default function CreateNewMessageForm(){
+    return(
+        <div className="DnD__CreateNewMessageForm">
+            <p>Create New Message Form</p>
+        </div>
+    )
+}
---
+++ react-app/src/components/modals/messages/CreateNewMessageModal.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./CreateNewMessageModal.css"
+export default function CreateNewMessageModal(){
+    return(
+        <div className="DnD__CreateNewMessageModal">
+            <p>Create New Message Modal</p>
+        </div>
+    )
+}
