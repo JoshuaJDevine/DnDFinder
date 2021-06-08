5529bbc481e8d93b363651b520027d2318876008
JoshuaJDevine <jd8386@gmail.com>
2021-06-08 13:20:21 -0700

    Added applicant state and routes

--- react-app/src/components/applications/application.js
+++ react-app/src/components/applications/application.js
@@ -1,8 +1,20 @@
-import React from "react";
-
+import React, {useEffect} from "react";
 import "./application.css"
+import {getApplicant} from "../../store/session";
+import {useDispatch} from "react-redux";
 export default function Application({applicationData}){
+
+    //Use this in the modal to get the applicant data
+    // const dispatch = useDispatch()
+    // console.log(applicationData)
+    //
+    // useEffect(async () => {
+    //     await dispatch(getApplicant(applicationData.userId));
+    // }, [])
+
     return(
-        <div className="DnD__application">{applicationData.userID} has applied with a status of {applicationData.status}</div>
+        <div className="DnD__application">
+            <p>{applicationData.userId} has applied with a status of {applicationData.status.toString()}</p>
+        </div>
     )
 }
