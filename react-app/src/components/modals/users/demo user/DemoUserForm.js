260f6031ed6f2812e7e9e77c3230a3141fba02a5
JoshuaJDevine <jd8386@gmail.com>
2021-06-10 13:53:15 -0700

    updated login demo

--- react-app/src/components/modals/users/demo user/DemoUserForm.js
+++ react-app/src/components/modals/users/demo user/DemoUserForm.js
@@ -21,9 +21,8 @@ export default function DemoUserForm( {setShowModal, setViewingGroup} ){
     }
     return(
         <div className="DnD__DemoUserForm">
-            <p>Welcome</p>
-            <p>This app is a work in progress</p>
-            <p>The Demo user can create groups, delete groups and edit groups</p>
+            <p>Welcome to the demo of DnD Group Finder</p>
+            <p>This app is [WIP]</p>
             <div className="DnD__Button--glow">
               <div className="DnD_Button--sign fx2" onClick={handleOK}>Login</div>
               <div className="DnD_Button--sign fx1" onClick={handleCancel}>Cancel</div>
